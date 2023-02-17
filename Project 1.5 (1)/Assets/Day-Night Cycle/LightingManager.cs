69b9244a40296723365fdf5f4f42e04c330cfe04
James Shephard <Jshephard150@gmail.com>
2023-02-17 10:27:43 +0000

    Lighting Controller Updates
    The lighting controller has been updated, so that it can turn on and off floodlights, and the headlights of the car when it gets dark

--- Project 1.5 (1)/Assets/Day-Night Cycle/LightingManager.cs
+++ Project 1.5 (1)/Assets/Day-Night Cycle/LightingManager.cs
@@ -1,4 +1,7 @@
+using System;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 [ExecuteAlways]
 public class LightingManager : MonoBehaviour
@@ -6,8 +9,15 @@ public class LightingManager : MonoBehaviour
     //Scene References
     [SerializeField] private Light DirectionalLight;
     [SerializeField] private LightingPreset Preset;
+
     //Variables
     [SerializeField, Range(0, 120)] private float TimeOfDay;
+    public Light[] lights;      // an array of lights to control
+    public float turnOnTime;    // the time of day to turn the lights on (in hours)
+    public float turnOffTime;   // the time of day to turn the lights off (in hours)
+
+    public Light FloodLight_1, FloodLight_2, FloodLight_3, FloodLight_4, FloodLight_5, FloodLight_6, FloodLight_;
+    public Light CarHeadLight_L, CarHeadLight_R;
 
 
     private void Update()
@@ -19,7 +29,7 @@ public class LightingManager : MonoBehaviour
         {
             //(Replace with a reference to the game time)
             TimeOfDay += Time.deltaTime;
-            TimeOfDay %= 120; //Modulus to ensure always between 0-24
+            TimeOfDay %= 120;       //Modulus to ensure always between 0-24
             UpdateLighting(TimeOfDay / 120f);
         }
         else
@@ -73,4 +83,38 @@ public class LightingManager : MonoBehaviour
         }
     }
 
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(TurnLightsOnAndOff());
+    }
+
+    IEnumerator  TurnLightsOnAndOff()
+    {
+        while (true)
+        {
+
+
+            // if it's past the turn on time and before the turn off time, turn on the lights
+            if (TimeOfDay > turnOnTime && TimeOfDay < turnOffTime)
+            {
+                foreach (Light light in lights)
+                {
+                    light.enabled = true;
+                }
+            }
+            // otherwise, turn off the lights
+            else
+            {
+                foreach (Light light in lights)
+                {
+                    light.enabled = false;
+                }
+            }
+
+            // wait for a minute before checking the time again
+            yield return new WaitForSeconds(2f);
+        }
+    }
 }
+
