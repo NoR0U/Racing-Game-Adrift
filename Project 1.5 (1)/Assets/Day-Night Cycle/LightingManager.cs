25424a918738deb4c7127b1db32fccf5b91dd32f
NoRou <120832022+NoR0U@users.noreply.github.com>
2023-02-20 18:19:08 +0000

    Revert "Merge pull request #7 from NoR0U/revert-4-Lighting-Updates-and-Related-Features"
    This reverts commit 4b0257f5d2295b2e00850e2d2ee00a294ae74432, reversing
    changes made to db977dd29584c3c27954a08903b4a7c27419692e.

--- Project 1.5 (1)/Assets/Day-Night Cycle/LightingManager.cs
+++ Project 1.5 (1)/Assets/Day-Night Cycle/LightingManager.cs
@@ -1,4 +1,7 @@
+using System;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 [ExecuteAlways]
 public class LightingManager : MonoBehaviour
@@ -6,8 +9,15 @@ public class LightingManager : MonoBehaviour
     //Scene References
     [SerializeField] private Light DirectionalLight;
     [SerializeField] private LightingPreset Preset;
+
     //Variables
     [SerializeField, Range(0, 120)] private float TimeOfDay;
+
+    public float turnOnTime;    // the time of day to turn the lights on (in hours)
+    public float turnOffTime;   // the time of day to turn the lights off (in hours)
+
+    public Light FloodLight_1, FloodLight_2, FloodLight_3, FloodLight_4, FloodLight_5, FloodLight_6, FloodLight_7;
+    public Light CarHeadLight_L, CarHeadLight_R;
 
 
     private void Update()
@@ -19,12 +29,41 @@ public class LightingManager : MonoBehaviour
         {
             //(Replace with a reference to the game time)
             TimeOfDay += Time.deltaTime;
-            TimeOfDay %= 120; //Modulus to ensure always between 0-24
+            TimeOfDay %= 120;       //Modulus to ensure always between 0-24
             UpdateLighting(TimeOfDay / 120f);
         }
         else
         {
             UpdateLighting(TimeOfDay / 120f);
+        }
+
+        // if it's past the turn on time and before the turn off time, turn on the lights
+        if (TimeOfDay > turnOnTime || TimeOfDay < turnOffTime)
+        {
+            FloodLight_1.enabled = true;
+            FloodLight_2.enabled = true;
+            FloodLight_3.enabled = true;
+            FloodLight_4.enabled = true;
+            FloodLight_5.enabled = true;
+            FloodLight_6.enabled = true;
+            FloodLight_7.enabled = true;
+
+            CarHeadLight_L.enabled = true;
+            CarHeadLight_R.enabled = true;
+        }
+        // otherwise, turn off the lights
+        else
+        {
+            FloodLight_1.enabled = false;
+            FloodLight_2.enabled = false;
+            FloodLight_3.enabled = false;
+            FloodLight_4.enabled = false;
+            FloodLight_5.enabled = false;
+            FloodLight_6.enabled = false;
+            FloodLight_7.enabled = false;
+
+            CarHeadLight_L.enabled = false;
+            CarHeadLight_R.enabled = false;
         }
     }
 
@@ -72,5 +111,5 @@ public class LightingManager : MonoBehaviour
             }
         }
     }
-
 }
+
