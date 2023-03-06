5208e170beee388c44dcb4b0eb5baac78dbb444c
NoRou <120832022+NoR0U@users.noreply.github.com>
2023-03-06 22:22:18 +0000

    Lap Timer
    The code for the lap timer is starting to be implemented and the track has now been split into the 4 different configurations in 4 different scenes so they can be loaded in septately

---
+++ Project 1.5 (1)/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public enum ControlType {  HumanInput, AI}
+    public ControlType controlType = ControlType.HumanInput;
+
+    public float BestLapTime { get; private set; } = Mathf.Infinity;
+    public float LastLapTime { get; private set; } = 0;
+    public float CurrentLapTime { get; private set; } = 0;
+    public int CurrentLap { get; private set; } = 0;
+
+    private float lapTimer;
+    private int lastCheckpointPassed = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
